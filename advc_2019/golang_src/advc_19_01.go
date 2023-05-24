fece8436c8e5034bf14511fce626e684b05f19f6
taehokangithub <95420530+taehokangithub@users.noreply.github.com>
2023-05-24 11:09:12 +0100

    day 01

--- advc_2019/golang_src/advc_19_01.go
+++ advc_2019/golang_src/advc_19_01.go
@@ -3,17 +3,48 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
-	"os"
+	"strconv"
+	"strings"
 )
 
 func get_fuel_requirement(mass int) int {
-	return mass*3 - 2
+	return mass/3 - 2
+}
+
+func solve_19_01_A(lines []string) int {
+	result := 0
+	for _, line := range lines {
+		mass, _ := strconv.Atoi(line)
+		fuel := get_fuel_requirement(mass)
+		result += fuel
+	}
+	return result
+}
+
+func solve_19_01_B(lines []string) int {
+	result := 0
+	for _, line := range lines {
+		mass, _ := strconv.Atoi(line)
+
+		for {
+			fuel := get_fuel_requirement(mass)
+			if fuel <= 0 {
+				break
+			}
+			result += fuel
+			//fmt.Println("mass ", mass, "requires fuel", fuel, "total", result)
+			mass = fuel
+		}
+	}
+	return result
 }
 
 func solve_19_01() {
-	fmt.Println("Hello")
-	file, _ := os.Open("../data/input01.txt")
-	binary, _ := ioutil.ReadAll(file)
+	content, _ := ioutil.ReadFile("../data/input01.txt")
 
-	fmt.Println(binary)
+	lines := strings.Split(string(content), "\n")
+
+	fmt.Println("ans1", solve_19_01_A(lines))
+
+	fmt.Println("ans2", solve_19_01_B(lines))
 }
---
+++ advc_2019/golang_src/file_reading_sample.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io/ioutil"
+	"os"
+)
+
+const file_name = "../data/input01.txt"
+
+func read_file_sample_1() {
+	fmt.Println("Sample reading version 1", file_name)
+
+	file, _ := os.Open(file_name)
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		fmt.Println(scanner.Text())
+	}
+}
+
+func read_file_sample_2() {
+	fmt.Println("Sample reading version 2", file_name)
+	content, _ := ioutil.ReadFile(file_name)
+	fmt.Println(string(content))
+}
+func file_reading_sample() {
+	read_file_sample_1()
+	read_file_sample_2()
+
+}
