e3640b1989f92648c2960d58d3d3816e17be7bf8
Taeho Kang <vegabird@gmail.com>
2023-06-15 22:36:02 +0100

    DAY18 removed V2

--- advc_2019/golang/day18/search.go
+++ advc_2019/golang/day18/search.go
@@ -1,6 +1,14 @@
 package day18
 
+import "taeho/advc19_go/utils"
+
 var SearchMH *searchMoveHeap
+
+type move struct {
+	myLocIndex int
+	loc        utils.Vector
+	steps      int
+}
 
 func (k *keyGrid) FindMinSteps() int {
 	SearchMH = newSearchMoveHeap()
@@ -59,101 +67,3 @@ func (k *keyGrid) findPossibleMovesV3() []*keyGrid {
 
 	return ret
 }
-
-/* Proved to be slower - but leaving here for reference
-var MovePool *utils.MemoryPool[move]
-var SearchRQ *utils.RingQueue[*move]
-// Also needs these initialisations somewhere
-	//MovePool = utils.NewMemoryPool[move](k.grid.Size.X * k.grid.Size.Y * 20)
-	//SearchRQ = utils.NewRingQueue[*move](k.grid.Size.X * k.grid.Size.Y)
-
-func (k *keyGrid) findPossibleMovesV2() []*keyGrid {
-	ret := make([]*keyGrid, 0, 64)
-	search := newSearchSet(k)
-	MovePool.Reset()
-	for i, myloc := range k.state.myLocs {
-		search.thisMove = &move{
-			myLocIndex: i,
-			loc:        myloc,
-			steps:      0,
-		}
-
-		search.findNextMovesV2()
-	}
-
-	for _, mv := range *search.retMoves {
-		ck := k.Copy()
-		tile := ck.grid.Get(mv.loc)
-		ck.SetKeyUnlocked(tile, true)
-		ck.steps += mv.steps
-		ck.state.myLocs[mv.myLocIndex] = mv.loc
-		ret = append(ret, ck)
-	}
-	return ret
-}
-
-func (search *searchSet) findNextMovesV2() {
-	sq := SearchRQ
-	sq.Clear()
-
-	sq.Push(search.thisMove)
-	possibleMoves := make([]*move, 0, 4)
-
-	for !sq.IsEmpty() {
-		thisMove := sq.Pop()
-		search.setVisited(&thisMove.loc)
-		thisMove.steps++
-		possibleMoves = possibleMoves[:0]
-
-		for _, dvec := range utils.DIR_VECTORS {
-			nextLoc := &utils.Vector{
-				X: thisMove.loc.X + dvec.X,
-				Y: thisMove.loc.Y + dvec.Y,
-			}
-			tile := search.k.grid.GetFast(nextLoc)
-			if tile == TILE_WALL {
-				continue
-			}
-			if search.isVisited(nextLoc) {
-				continue
-			}
-			nextMove := MovePool.New()
-			nextMove.myLocIndex = thisMove.myLocIndex
-			nextMove.steps = thisMove.steps
-			nextMove.loc = *nextLoc
-
-			if tile == TILE_EMPTY || search.checkIfPossibleMove(nextMove, tile) {
-				possibleMoves = append(possibleMoves, nextMove)
-			}
-		}
-
-		if search.hasSearchFinished() {
-			return
-		}
-
-		for i := 0; i < len(possibleMoves); i++ {
-			sq.Push(possibleMoves[i])
-		}
-	}
-}
-
-func (search *searchSet) checkIfPossibleMove(thisMove *move, tile rune) bool {
-	k := search.k
-
-	if k.IsDoor(tile) { // Door found
-		return k.HasKeyUnlocked(tile)
-	}
-
-	if k.IsKey(tile) && !k.HasKeyUnlocked(tile) { // Key found
-		if existingMove, exists := (*search.retMoves)[tile]; exists {
-			if existingMove.steps <= thisMove.steps {
-				return false // discard this move
-			}
-		}
-		(*search.retMoves)[tile] = thisMove // Save this move as a key-founder
-		return false                        // but return false, should not go further
-	}
-
-	return true
-}
-*/
