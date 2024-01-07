a651c2df444d30f94077c35aae1f52f37fc43507
Taeho Kang <vegabird@gmail.com>
2024-01-07 22:09:06 +0000

    Delete advc_2019/dotnet directory

--- advc_2019/dotnet/src/utils/GenericUtil.cs
+++
@@ -1,8 +0,0 @@
-namespace Advc.Utils
-{
-    class GenericUtil
-    {
-        public static char IntTochar(int val) => (char) (val + '0');
-        public static int CharToInt(char c) => (int) (c - '0');
-    }
-}
